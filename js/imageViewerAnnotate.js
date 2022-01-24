28612bce710a7a12e5b42d6777d61f4d0cc2a975
mattsliv <msliv12275@gmail.com>
2022-01-24 11:06:04 -0500

    feat: apply container styling for portrait images

--- js/imageViewerAnnotate.js
+++ js/imageViewerAnnotate.js
@@ -227,6 +227,27 @@ IVEM.insertPreview = function (field, params) {
     // Piping - get target containers
     $container = $("div[data-ivem-pipe-source=" + params.pipe_source + "]");
   }
+
+// If an image is loaded as portrait, make styling adjustments to the image's container
+let is_portrait = null;
+let img = document.createElement('img');
+img.src = src;
+// Poll image to get its dimensions
+let img_poll = setInterval(function () {
+  if (img.naturalWidth) {
+      clearInterval(img_poll);
+      is_portrait = img.naturalWidth < img.naturalHeight;
+  }
+}, 10);
+// Apply stylng
+img.onload = function () {
+  if (is_portrait) {
+    $container.each( function () {
+      $(this).css("max-width", "max(50vw, 250px)");
+    });
+  }
+}
+
   $container.each(function () {
     $this_cont = $(this);
     // Create a new image element
